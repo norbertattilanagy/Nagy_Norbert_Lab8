390faad37f2aad58805196cf56b9de047acdf036
norbertattilanagy <norbertattilanagy@gmail.com>
2021-11-25 20:31:27 +0200

    Add project files.

---
+++ Data/Nagy_Norbert_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Nagy_Norbert_Lab8.Models;
+
+namespace Nagy_Norbert_Lab8.Data
+{
+    public class Nagy_Norbert_Lab8Context : DbContext
+    {
+        public Nagy_Norbert_Lab8Context (DbContextOptions<Nagy_Norbert_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Nagy_Norbert_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Nagy_Norbert_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Nagy_Norbert_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Nagy_Norbert_Lab8.Data;
+using Nagy_Norbert_Lab8.Models;
+
+namespace Nagy_Norbert_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Nagy_Norbert_Lab8.Data.Nagy_Norbert_Lab8Context _context;
+
+        public IndexModel(Nagy_Norbert_Lab8.Data.Nagy_Norbert_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.Include(b => b.Publisher).ToListAsync();
+        }
+    }
+}
